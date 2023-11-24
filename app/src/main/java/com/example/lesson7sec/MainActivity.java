d879908949aba8e4ce7153d04752bcb79cea231e
xiaoeeyu <xiaoeryu@163.com>
2023-11-23 21:57:46 -0500

    Initial commit

---
+++ app/src/main/java/com/example/lesson7sec/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.lesson7sec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText username_et;
+    EditText password_et;
+    TextView message_tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        password_et = (EditText) this.findViewById(R.id.editText2);
+        username_et = (EditText) this.findViewById(R.id.editText);
+        message_tv = ((TextView) findViewById(R.id.textView));
+
+        this.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (username_et.getText().toString().compareTo("admin") == 0) {
+                    message_tv.setText("You cannot login as admin");
+                    return;
+                }
+                //hook target
+                message_tv.setText("Sending to the server :" + Base64.encodeToString((username_et.getText().toString() + ":" + password_et.getText().toString()).getBytes(), Base64.DEFAULT));
+            }
+        });
+    }
+}
